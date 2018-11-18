e0ef800b93043d9785795736cca4795189339954
fuzzy-coder <joshi.aditya402@gmail.com>
2018-11-18 15:55:53 +0530

    -- added UserContacts model and api -- deleting user_contacts upon user deletion

--- models/users.js
+++ models/users.js
@@ -2,48 +2,97 @@ const mongoose = require('mongoose')
 const bcrypt = require('mongoose-bcrypt');
 const timestamps = require('mongoose-timestamp');
 
-const UserSchema = new mongoose.Schema(
-        {
-                name: {
-                    type: String,
-                    trim: true,
-                    required: true,
-                },
-                email: {
-                        type: String,
-                        lowercase: true,
-                        trim: true,
-                        index: true,
-                        unique: true,
-                        required: true,
-                },
-                password: {
-                        type: String,
-                        required: true,
-                        bcrypt: true,
-                },
-                address: {
-                        type: String,
-                        trim: true,
-                        required: true,
-                },
-                phone: {
-                        type: String,
-                        trim: true,
-                        required: true,
-                },
-                dob: {
-                        type: Date,
-                        required: false,
-                        default: null
-                }
+const UserSchema = new mongoose.Schema({
+        name: {
+                type: String,
+                trim: true,
+                required: true,
         },
-        { collection: 'users' },
-);
+        email: {
+                type: String,
+                lowercase: true,
+                trim: true,
+                index: true,
+                required: true,
+        },
+        password: {
+                type: String,
+                required: true,
+                bcrypt: true,
+        },
+        address: {
+                type: String,
+                trim: true,
+                required: true,
+        },
+        phone: {
+                type: String,
+                trim: true,
+                required: true,
+        },
+        dob: {
+                type: Date,
+                required: false,
+                default: null
+        },
+        contacts: [{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'UserContact'
+        }]
+}, {
+        collection: 'users'
+}, );
 
 UserSchema.plugin(bcrypt);
 UserSchema.plugin(timestamps);
+UserSchema.index({
+        email: 1,
+        phone: 1
+}, {
+        unique: true
+}); // compound index on email + username
 
-UserSchema.index({ email: 1, phone: 1 }); // compound index on email + username
+const UserContactSchema = new mongoose.Schema({
+        name: {
+                type: String,
+                trim: true,
+                required: true,
+        },
+        email: {
+                type: String,
+                lowercase: true,
+                trim: true,
+                index: true,
+                required: true,
+        },
+        phone: {
+                type: String,
+                trim: true,
+                index: true,
+                required: true,
+        },
+        user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+        }
+}, {
+        collection: 'user_contacts'
+}, );
 
-module.exports = exports = mongoose.model('User', UserSchema); // export model for use
+UserContactSchema.index({
+        email: 1,
+        phone: 1
+}, {
+        unique: true
+});
+
+UserSchema.pre('remove', function (next) {
+        this.model('UserContact').remove({
+                user: this._id
+        }, next);
+})
+
+module.exports = {
+        User: mongoose.model('User', UserSchema),
+        UserContact: mongoose.model('UserContact', UserContactSchema)
+}
--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,81 @@
 let express = require('express');
 let router = express.Router();
 
-const ResponseHandler =  require('../util/reponse')
+const ResponseHandler = require('../util/reponse')
 
-const User = require('../models/users')
+const {
+  User,
+  UserContact
+} = require('../models/users')
 
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res) => {
   try {
-    let users = await User.find()
-    ResponseHandler.ok(res, users)  
+    let users = await User.find().populate('contacts')
+    ResponseHandler.ok(res, users)
   } catch (error) {
     ResponseHandler.serverError(res, error)
   }
 });
 
-router.get('/:user_id', async (req, res)=>{
-    try {
-      let user = await User.findById(req.params.user_id)
-      if(!user){
-          return ResponseHandler.notFound(res, `Cannot find user for user_id :: ${req.params.user_id}`)
-      }
-      ResponseHandler.ok(res, user)  
-    } catch (error) {
-      ResponseHandler.serverError(res, error)
+router.get('/:user_id', async (req, res) => {
+  try {
+    let user = await User.findById(req.params.user_id)
+    if (!user) {
+      return ResponseHandler.notFound(res, `Cannot find user for user_id :: ${req.params.user_id}`)
     }
+    ResponseHandler.ok(res, user)
+  } catch (error) {
+    ResponseHandler.serverError(res, error)
+  }
 });
 
-router.post('/', async (req, res)=>{
-    let body = req.body
-    try {
-      let user = await User.create(body)  
-      ResponseHandler.ok(res, user)  
-    } catch (error) {
-      ResponseHandler.serverError(res, error)
+router.post('/', async (req, res) => {
+  let body = req.body
+  try {
+    let user = await User.create(body)
+    ResponseHandler.ok(res, user)
+  } catch (error) {
+    ResponseHandler.serverError(res, error)
+  }
+});
+
+router.put('/:user_id/contact', async (req, res) => {
+  let contact = req.body
+  try {
+    let user = await User.findById(req.params.user_id)
+
+    if (!user) {
+      return ResponseHandler.notFound(res, `Cannot find user for user_id :: ${req.params.user_id}`)
     }
+
+    contact.user = user._id
+
+    let userContact = await UserContact.create(contact)
+
+    user.contacts.push(userContact._id)
+    await user.save()
+
+    ResponseHandler.ok(res, userContact)
+  } catch (error) {
+    ResponseHandler.serverError(res, error)
+  }
 });
 
-router.put('/contact', async (req, res)=>{
-  res.json({user_id: req.param('user_id')});
-});
+router.delete('/:user_id', async (req, res) => {
+  try {
+    let user = await User.findById(req.params.user_id)
 
-router.delete('/contact', async (req, res)=>{
-  res.json({user_id: req.param('user_id')});
-});
+    if (!user) {
+      return ResponseHandler.notFound(res, `Cannot find user for user_id :: ${req.params.user_id}`)
+    }
 
-router.delete('/:user_id', async (req, res)=>{
-  res.json({user_id: req.param('user_id')});
+    await user.remove()
+
+    ResponseHandler.ok(res, "User deleted successfully")
+  } catch (error) {
+    ResponseHandler.serverError(res, error)
+  }
+
 });
 
 module.exports = router;

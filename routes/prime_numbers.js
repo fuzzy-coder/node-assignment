e696a3ca819191b92c390cefb02ca73f3d07f0c1
fuzzy-coder <joshi.aditya402@gmail.com>
2018-11-19 08:56:35 +0530

    -- added basic validation to prime numbers

--- routes/prime_numbers.js
+++ routes/prime_numbers.js
@@ -3,27 +3,34 @@ let router = express.Router();
 
 let NumberUtil = require('../util/number')
 let NativeNumberUtil = require('../build/Release/addon')
+let ResponseHandler = require('../util/reponse')
 
 router.get('/node/:last', async(req, res, next)=>{
   let store = [];
   let last = req.params.last || 0
+  if(!/[0-9]+/.test(last) || last < 2){
+    return ResponseHandler.badRequest(res, "Input parameter should be a valid number greater than 2")
+  }
   for(let i=2; i < last; i++){
       if(NumberUtil.isPrime(i)){
         store.push(i)
       }
   }
-  res.json(store);
+  ResponseHandler.ok(res, store)
 });
 
 router.get('/native/:last', async(req, res, next)=>{
   let store = [];
   let last = req.params.last || 0
+  if(!/[0-9]+/.test(last) || last < 2){
+    return ResponseHandler.badRequest(res, "Input parameter should be a valid number greater than 2")
+  }
   for(let i=2; i < last; i++){
       if(NativeNumberUtil.IsPrime(i)){
         store.push(i)
       }
   }
-  res.json(store);
+  ResponseHandler.ok(res, store)
 });
 
 module.exports = router;
